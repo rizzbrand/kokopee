4d1a124f38b9d6f4e4f0447f03af56dd42ecfffd
Harnish <harrnish@gmail.com>
2024-11-26 22:12:23 -0500

    Hero section

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Transition from "../../components/transition/Transition";
+import { Link } from "react-router";
 
 import "./Home.css";
 
@@ -9,6 +10,28 @@ const Home = () => {
       <section className="hero">
         <div className="hero-img">
           <img src="/home/hero.jpg" alt="" />
+        </div>
+
+        <div className="hero-header">
+          <h1>
+            Balanced <br /> Pitch
+          </h1>
+          <p>In the Dawn of AI Revolution</p>
+          <button>
+            <Link to="/contact">Get in touch</Link>
+          </button>
+        </div>
+
+        <div className="news-article">
+          <div className="news-article-title">
+            <p className="primary">
+              Riaa takes on suno and udio: ai <br /> copyright clash
+            </p>
+          </div>
+          <div className="news-article-info">
+            <p>7.1.2024</p>
+            <p>News</p>
+          </div>
         </div>
       </section>
     </div>
